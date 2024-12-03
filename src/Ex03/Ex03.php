70c5bc22e7be54e9d84e3f59cdfa7830e2e7d04f
Arne Meier <arne@zone.ee>
2024-12-03 16:14:02 +0200

    Ex03, setup for ex04

--- src/Ex03/Ex03.php
+++ src/Ex03/Ex03.php
@@ -5,7 +5,7 @@ namespace Aoc2024\Ex03;
 use Aoc2024\Main\Exercise;
 
 /**
- * 
+ * too low: 26922215
  */
 class Ex03 extends Exercise
 {
@@ -17,6 +17,39 @@ class Ex03 extends Exercise
 
     public function run($arr)
     {
-        return $arr;
+        $str = implode('', $arr);
+
+        // part 1
+        $res = $this->getMultipliedFrom($str);
+
+        // part 2
+        $res = 0;
+
+        preg_match_all('/(?:do\(\))|(?:don\'t\(\))/', $str, $dosAndDonts);
+        $splat = preg_split('/(?:do\(\))|(?:don\'t\(\))/', $str);
+
+        array_unshift($dosAndDonts[0], 'do()');
+
+        foreach ($dosAndDonts[0] as $key => $value) {
+            if ('do()' == $value) {
+                $res += $this->getMultipliedFrom($splat[$key]);
+            }
+        }
+
+        return $res;
+    }
+
+    public function getMultipliedFrom($inputString)
+    {
+        $res = 0;
+
+        preg_match_all('/mul\(\d+,\d+\)/m', $inputString, $matches);
+
+        foreach ($matches[0] as $match) {
+            preg_match_all('/(\d+),(\d+)/', $match, $numbers);
+            $res += ($numbers[1][0] * $numbers[2][0]);
+        }
+
+        return $res;
     }
 }
