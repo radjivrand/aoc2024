6babcd6d698aab5d3f4c9f663e93179411879855
Arne <arne@loremipsum.ee>
2024-12-07 21:53:26 +0200

    Ex07 part 1

--- src/Ex07/Ex07.php
+++ src/Ex07/Ex07.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace Aoc2024\Ex09;
+namespace Aoc2024\Ex07;
 
 use Aoc2024\Main\Exercise;
 
 /**
  * 
  */
-class Ex09 extends Exercise
+class Ex07 extends Exercise
 {
+    public array $eqs = [];
+
     public function __construct()
     {
         $inputArr = $this->getFileContents();
@@ -17,6 +19,63 @@ class Ex09 extends Exercise
 
     public function run($arr)
     {
-        return $arr;
+        foreach ($arr as $line) {
+            $splat = preg_split('/(:?\s)/', $line);
+            $index = $splat[0];
+            array_shift($splat);
+            $this->eqs[$index] = $splat;
+        }
+
+        $calibration = 0;
+        foreach ($this->eqs as $result => $values) {
+            if ($this->findResultWithValues($result, $values)) {
+                $calibration += $result;
+            }
+        }
+
+        return $calibration;
+    }
+
+    public function findResultWithValues($result, $values): bool
+    {
+        $possibilities = $this->getOperatorOptions($values);
+
+        foreach ($possibilities as $operators) {
+            $product = $values[0];
+
+            foreach (str_split($operators) as $key => $sign) {
+                eval('$product = $product' . $sign . $values[$key + 1] . ';');
+
+                if ($product > $result) {
+                    continue 2;
+                }
+            }
+
+            if ($result == $product) {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+
+    public function getOperatorOptions($values)
+    {
+        $amount = 2 ** (count($values) - 1);
+        $len = base_convert($amount, 10, 2);
+
+        $options = [];
+
+        for ($i=0; $i < $amount; $i++) {
+            $val = base_convert($i, 10, 2);
+            $options[] = str_pad($val, strlen($len - 1), '0', STR_PAD_LEFT);
+        }
+
+        foreach ($options as &$option) {
+            $option = str_replace(['0' , '1'], ['+', '*'], $option);
+        }
+
+        return $options;
     }
 }
